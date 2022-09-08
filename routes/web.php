77e9091dc168712010fc9df8e3cb1f1e54a6db50
marcorlino <marcangelo.orlino.s@bulsu.edu.ph>
2022-09-08 20:53:39 +0800

    First commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Firebase\ContactController;
+
+
+Route::get('contacts', [ContactController::class, 'index']); 
+
+Route::get('add-contact', [ContactController::class, 'create']); 
+
+Route::post('add-contact', [ContactController::class, 'store']);  
+
+Route::get('edit-contact/{id}', [ContactController::class, 'edit']);  
+
+Route::put('update-contact/{id}', [ContactController::class, 'update']);  
+
+// Route::get('delete-contact/{id}', [ContactController::class, 'destroy']);  
+Route::delete('delete-contact/{id}', [ContactController::class, 'destroy']);  
+
+Route::get('/', function () {
+    return view('welcome');
+});
